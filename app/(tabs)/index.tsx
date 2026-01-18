05cc4a78c0625e48977a85e41efeffd2f597339c
Samarth Puri <connectwsam@gmail.com>
2026-01-18 15:10:42 -0500

    Update Bluetooth scanning UI and dependencies

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,9 +1,10 @@
 import React, { useState } from 'react';
-import { Button, PermissionsAndroid, View } from "react-native";
+import { Button, FlatList, PermissionsAndroid, Text, View } from "react-native";
 import RNBluetoothClassic from "react-native-bluetooth-classic";
 
 export default function Index() {
 
+  //let i = 0
 async function requestPermission(){
   const granted = await PermissionsAndroid.requestMultiple([
     PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
@@ -12,16 +13,38 @@ async function requestPermission(){
   ])
 }
 
+
 async function scan(){
           const devices = await RNBluetoothClassic.startDiscovery();
           const classicdevs = devices.filter(device => device.type == "DUAL" )
           console.log("FOUND CLASSIC DEVICES:", classicdevs);
-          return classicdevs
+          setDevices(classicdevs)
+          //return classicdevs
         }
 
+async function connectDevice(device: any){
+    //setStatus("active")
+    try{
+      await device.connect()
+      setConnection("successful")
+      device.onDataReceived((event: any) => {
+      console.log(event.data);
+    });
+    }
+    catch(error){
+      setConnection("failed")
+    }
+    
+    
+    
+    // console.log("You have now connected to your device.")
+}
 
 
+  const [devices, setDevices] = useState<any[]>([]);
   const [noaudio, setnoaudio] = useState(true)
+  const [connection, setConnection] = useState("null") //connect is var name and is initalized to connecting..., 
+                                                          //update value (connect) using 'setconnect'
   
   return (
     <View style={{ flex: 1, backgroundColor: "white", padding: 60 }}>
@@ -31,8 +54,21 @@ async function scan(){
         setnoaudio(false); //sets no audio to false
         await requestPermission()
         await scan()
-        
         }}/>
+      <FlatList 
+        data = {devices}
+        renderItem={({item}) => 
+        (
+          <Button title={item.name}
+           onPress = {async () => {
+           await connectDevice(item)
+           
+           }}/>
+          
+
+        )}  />
+        <Text>Connection {connection}</Text>
+
 
         
         
@@ -42,4 +78,5 @@ async function scan(){
 
     </View>
   );
+
 }
